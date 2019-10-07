7ce7da3bfca6f59beb9115826b676b4f764c2f5f
hanwuji621 <40812729+hanwuji621@users.noreply.github.com>
2019-10-06 17:47:32 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+window.addEventListener('load',()=>{
+    let long;
+    let lat;
+
+    if(navigator.geolocation)
+    {
+        navigator.geolocation.getCurrentPosition(position =>{
+            console.log(position);
+            long = position.coords.longitude;
+            lat  = position.coords.latitude;
+        });
+    }
+    else
+    {
+        h1.textContent = "hey this is not working because reasons";
+    }
+})

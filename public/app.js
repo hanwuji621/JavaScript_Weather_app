4c53f71aded8c4543f64581ff95f7b37cee47c51
hanwuji621 <40812729+hanwuji621@users.noreply.github.com>
2019-10-30 10:36:32 -0700

    Add files via upload

---
+++ public/app.js
@@ -0,0 +1,26 @@
+window.addEventListener('load',()=>{
+    let long;
+    let lat;
+    
+    if(navigator.geolocation)
+    {
+        navigator.geolocation.getCurrentPosition(position =>{
+            console.log(position);
+            long = position.coords.longitude;
+            lat  = position.coords.latitude;
+
+            const proxy ="https://cors-anywhere.herokuapp.com";
+            const api = '${proxy}https://api.darksky.net/forecast/413348a644267a7ec177319ea973a021/${lat},${long}';
+
+            fetch(api)
+                .then(Response =>{
+                    //return Response.json();
+                    rconsole.log(Response);
+                });
+                //.then(data =>{
+                //console.log(data);
+            //});
+        });
+
+    }
+});

fb3579414e59b11146839e9f5da16d4be4236cbd
hanwuji621 <40812729+hanwuji621@users.noreply.github.com>
2019-10-30 10:58:59 -0700

    Update app.js

--- public/app.js
+++ public/app.js
@@ -10,7 +10,7 @@ window.addEventListener('load',()=>{
             lat  = position.coords.latitude;
 
             const proxy ="https://cors-anywhere.herokuapp.com";
-            const api = 'api.openweathermap.org/data/2.5/weather?lat=35&lon=139&APPID=17dd6c9ee5a497d16add578b6ad550fe';
+            const api = 'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&APPID=17dd6c9ee5a497d16add578b6ad550fe';
 
             fetch(api)
                 .then(Response =>{
